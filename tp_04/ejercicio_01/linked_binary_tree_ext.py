aced400fcb7027ec9e1444cb7daa13ed307d475a
Giov097 <gero.giovenale@gmail.com>
2022-11-03 20:54:39 -0300

    added unit tests

--- tp_04/ejercicio_01/linked_binary_tree_ext.py
+++ tp_04/ejercicio_01/linked_binary_tree_ext.py
@@ -3,38 +3,42 @@ from linked_binary_tree_ext_abstract import LinkedBinaryTreeExtAbstract
 from data_structures.trees.linked_binary_tree import LinkedBinaryTree
 from data_structures import BinaryTreeNode
 
+
 class LinkedBinaryTreeExt(LinkedBinaryTreeExtAbstract, LinkedBinaryTree):
+
     def hermanos(self, nodo1: BinaryTreeNode, nodo2: BinaryTreeNode) -> bool:
         return self._search_parent(nodo1) == self._search_parent(nodo2)
 
     def hojas(self) -> List[Any]:
-        list = []
-        def agregar_elementos_hoja(node: BinaryTreeNode, list : List) -> None:    
+        lista = []
+
+        def agregar_elementos_hoja(node: BinaryTreeNode, list: List) -> None:
             if node:
-                if (node.children_count() == 0):        
+                if (node.children_count() == 0):
                     list.append(node.element)
 
                 agregar_elementos_hoja(node.left_child, list)
                 agregar_elementos_hoja(node.right_child, list)
-        agregar_elementos_hoja(self._root, list)
-        return list
+        agregar_elementos_hoja(self._root, lista)
+        return lista
 
     def internos(self) -> List[Any]:
-        list = []
-        def agregar_elementos_interos(node: BinaryTreeNode, list : List) -> None:    
+        lista = []
+
+        def agregar_elementos_interos(node: BinaryTreeNode, list: List) -> None:
             if node:
                 if (self._search_parent(node) != None and (node.left_child != None or node.right_child != None)):
                     list.append(node.element)
 
                 agregar_elementos_interos(node.left_child, list)
                 agregar_elementos_interos(node.right_child, list)
-        agregar_elementos_interos(self._root, list)
-        return list
+        agregar_elementos_interos(self._root, lista)
+        return lista
 
     def profundidad(self, nodo: BinaryTreeNode) -> int:
-        def encontrar_profundidad(node: BinaryTreeNode) -> int:  
-            if node:               
-                if (self._search_parent(node) != None):    
+        def encontrar_profundidad(node: BinaryTreeNode) -> int:
+            if node:
+                if (self._search_parent(node) != None):
                     node = self._search_parent(node)
                     return 1 + encontrar_profundidad(node)
                 else:
@@ -43,10 +47,10 @@ class LinkedBinaryTreeExt(LinkedBinaryTreeExtAbstract, LinkedBinaryTree):
         return encontrar_profundidad(nodo)
 
     def altura(self, nodo: BinaryTreeNode) -> int:
-        def encontrar_altura(node: BinaryTreeNode) -> int:  
-            if node:               
-                if (node.left_child != None):  
-                    return 1 + encontrar_altura(node.right_child)         
+        def encontrar_altura(node: BinaryTreeNode) -> int:
+            if node:
+                if (node.left_child != None):
+                    return 1 + encontrar_altura(node.right_child)
                 elif (node.right_child != None):
                     return 1 + encontrar_altura(node.left_child)
                 else:
--- tp_04/ejercicio_01/linked_binary_tree_ext_client.py
+++ tp_04/ejercicio_01/linked_binary_tree_ext_client.py
@@ -1,45 +1,50 @@
 from data_structures import BinaryTreeNode
 from linked_binary_tree_ext import LinkedBinaryTreeExt
+from io import UnsupportedOperation
+import unittest
 
-nodo_a = BinaryTreeNode('A')
-nodo_b = BinaryTreeNode('B')
-nodo_c = BinaryTreeNode('C')
-nodo_d = BinaryTreeNode('D')
-nodo_f = BinaryTreeNode('F')
-nodo_g = BinaryTreeNode('G')
-nodo_h = BinaryTreeNode('H')
-nodo_i = BinaryTreeNode('I')
-nodo_k = BinaryTreeNode('K')
-nodo_m = BinaryTreeNode('M')
-nodo_n = BinaryTreeNode('N')
 
-arbol = LinkedBinaryTreeExt()
-arbol.add_left_child(None, nodo_a)
-arbol.add_left_child(nodo_a, nodo_b)
-arbol.add_right_child(nodo_a, nodo_f)
-arbol.add_left_child(nodo_b, nodo_c)
-arbol.add_right_child(nodo_b, nodo_d)
-arbol.add_left_child(nodo_f, nodo_g)
-arbol.add_right_child(nodo_f, nodo_k)
-arbol.add_left_child(nodo_g, nodo_h) 
-arbol.add_right_child(nodo_g, nodo_i)
-arbol.add_left_child(nodo_k, nodo_m)
-arbol.add_right_child(nodo_k, nodo_n)
+class TestLinkedStackExt(unittest.TestCase):
 
-print("Árbol: ")
-print(arbol)
+    nodo_a = BinaryTreeNode('A')
+    nodo_b = BinaryTreeNode('B')
+    nodo_c = BinaryTreeNode('C')
+    nodo_d = BinaryTreeNode('D')
+    nodo_f = BinaryTreeNode('F')
+    nodo_g = BinaryTreeNode('G')
+    nodo_h = BinaryTreeNode('H')
+    nodo_i = BinaryTreeNode('I')
+    nodo_k = BinaryTreeNode('K')
+    nodo_m = BinaryTreeNode('M')
+    nodo_n = BinaryTreeNode('N')
+    arbol = LinkedBinaryTreeExt()
+    arbol.add_left_child(None, nodo_a)
+    arbol.add_left_child(nodo_a, nodo_b)
+    arbol.add_right_child(nodo_a, nodo_f)
+    arbol.add_left_child(nodo_b, nodo_c)
+    arbol.add_right_child(nodo_b, nodo_d)
+    arbol.add_left_child(nodo_f, nodo_g)
+    arbol.add_right_child(nodo_f, nodo_k)
+    arbol.add_left_child(nodo_g, nodo_h)
+    arbol.add_right_child(nodo_g, nodo_i)
+    arbol.add_left_child(nodo_k, nodo_m)
+    arbol.add_right_child(nodo_k, nodo_n)
 
-print("\n¿Son hermanos?: ")
-print(arbol.hermanos(nodo_b, nodo_f))
+    def test_hermanos(self):
+        self.assertEqual(self.arbol.hermanos(self.nodo_b, self.nodo_f), True)
 
-print("\nElementos de nodos sin hijos: ")
-print(arbol.hojas())
+    def test_hojas(self):
+        self.assertEqual(self.arbol.hojas(), "['C', 'D', 'H', 'I', 'M', 'N']")
 
-print("\nElementos de los nodos internos: ")
-print(arbol.internos())
+    def test_internos(self):
+        self.assertEqual(self.arbol.internos(), "['B', 'F', 'G', 'K']")
 
-print("\nProfundidad del nodo: ")
-print(arbol.profundidad(nodo_g))
+    def test_profundidad(self):
+        self.assertEqual(self.arbol.profundidad(self.nodo_g), 2)
 
-print("\nAltura de nodos: ")
-print(arbol.altura(nodo_a))
+    def test_altura(self):
+        self.assertEqual(self.arbol.altura(self.nodo_a), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
